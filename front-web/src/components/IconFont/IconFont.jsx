31504b87246519f9953dceee474af95f106214a8
zhaotianzeng <tianzneg@gmail.com>
2021-12-15 17:09:56 +0800

    delete origin web

--- front-web/src/components/IconFont/IconFont.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import {createFromIconfontCN} from '@ant-design/icons';
-import defaultSettings from "../../../config/defaultSettings";
-
-const IconFont = createFromIconfontCN({
-  scriptUrl: defaultSettings.iconfontUrl,
-});
-export default IconFont
